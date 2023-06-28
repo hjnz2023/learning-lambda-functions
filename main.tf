235bfa37f7ebf03b47f082919df3f579abb1e4e1
hjnz <heft-kayaks.06@icloud.com>
2023-06-28 22:11:38 +1200

    refactor: Tidy up duplicated strings

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
+locals {
+  namespace = "experiment-01"
+}
+
 module "opensearch_cluster" {
   source      = "./modules/opensearch"
-  domain_name = "experiment-01"
+  domain_name = local.namespace
 }
 
 module "check_opensearch_snapshot_lambda" {
   source     = "./modules/scheduled-lambda-function"
-  namespace  = "experiment-01"
+  namespace  = local.namespace
   opensearch = module.opensearch_cluster.opensearch
 }
